6a0613a9c2b1d1af1e27de292dc2a3751dc205b4
TpaBKa251 <TpaBKa251@users.noreply.github.com>
2025-11-26 12:25:15 +0000

    deploy: 512eb68e18e5a714257aaf542d5946063dc7ad2b

--- hostel-common/package-search-index.js
+++ hostel-common/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.tpu.hostel.internal.exception"},{"l":"ru.tpu.hostel.internal.utils"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"ru.tpu.hostel.internal.exception"},{"l":"ru.tpu.hostel.internal.utils"}];updateSearchResults();
