f69a5652b6725dfe01c79560227ccc2f6271a1a1
Лапшин Илья Романович <irl1@tpu.ru>
2025-12-06 03:49:30 +0700

    Фикс

--- notification/src/main/java/ru/tpu/hostel/internal/HostelNotificationAutoConfiguration.java
+++ notification/src/main/java/ru/tpu/hostel/internal/HostelNotificationAutoConfiguration.java
@@ -3,14 +3,13 @@ package ru.tpu.hostel.internal;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Import;
 import ru.tpu.hostel.internal.builder.impl.DefaultNotificationRequestBuilder;
-import ru.tpu.hostel.internal.config.amqp.RabbitNotificationServiceConfig;
 import ru.tpu.hostel.internal.service.impl.DefaultNotificationSender;
 
 @AutoConfiguration
 @Import({
         DefaultNotificationRequestBuilder.class,
-        DefaultNotificationSender.class,
-        RabbitNotificationServiceConfig.class
+        DefaultNotificationSender.class
+        //RabbitNotificationServiceConfig.class
 })
 public class HostelNotificationAutoConfiguration {
 }
